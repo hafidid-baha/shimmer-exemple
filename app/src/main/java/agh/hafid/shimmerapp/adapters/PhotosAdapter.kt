cd5472566dabd098f68effcdeca8ba01fd21bcb7
hafid id-baha <hafidsoftdev@gmail.com>
2020-09-07 21:32:08 +0100

    update files

--- app/src/main/java/agh/hafid/shimmerapp/adapters/PhotosAdapter.kt
+++ app/src/main/java/agh/hafid/shimmerapp/adapters/PhotosAdapter.kt
@@ -1,4 +1,48 @@
 package agh.hafid.shimmerapp.adapters
 
-class PhotosAdapter {
+import agh.hafid.shimmerapp.R
+import android.content.Context
+import android.media.Image
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import org.json.JSONObject
+
+class PhotosAdapter(val data:MutableList<JSONObject>,val context:Context): RecyclerView.Adapter<PhotosAdapter.mViewHolder>() {
+
+    class mViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var image:ImageView
+        var text:TextView
+
+        init {
+            image = itemView.findViewById(R.id.image)
+            text = itemView.findViewById(R.id.text)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): mViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_item,parent,false)
+        return mViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onBindViewHolder(holder: mViewHolder, position: Int) {
+        val photo = data.get(position)
+        val image = photo.getString("thumbnailUrl");
+        val title = photo.getString("title")
+
+        Glide.with(context).load(image).into(holder.image)
+        holder.text.setText(title)
+
+
+    }
+
 }
