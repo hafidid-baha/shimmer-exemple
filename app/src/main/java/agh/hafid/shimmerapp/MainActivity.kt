7f68c5e70ad591e3fd748d3641d3d9727c93850d
hafid id-baha <hafidsoftdev@gmail.com>
2020-09-07 19:25:01 +0100

    start shimmer activity

--- app/src/main/java/agh/hafid/shimmerapp/MainActivity.kt
+++ app/src/main/java/agh/hafid/shimmerapp/MainActivity.kt
@@ -2,10 +2,24 @@ package agh.hafid.shimmerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.facebook.shimmer.Shimmer
+import com.facebook.shimmer.ShimmerFrameLayout
 
 class MainActivity : AppCompatActivity() {
+    lateinit var shimmer:ShimmerFrameLayout;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        shimmer = findViewById(R.id.shimmer)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        shimmer.startShimmer()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        shimmer.stopShimmer()
     }
 }
